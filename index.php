0006609f9032703a93deca24aad2927a4bed4382
ibreaker14 <minijordon@gmail.com>
2016-04-17 13:51:33 -0700

    style menu links

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
 </head>
 <body>
 <?php session_start(); ?>
+<span class="absolute right"><p>Call Us: +1 (562) 123-4567</p></span>
 <?php include 'navbar.php' ?><br>
   	<div id="content">
   		<main>
-  			<img src="images/logo.png" alt="logo">
+  			<!-- <img src="images/logo.png" alt="logo"> -->
   		</main>
   	</div><!-- end #content -->
   <?php include 'footer.php' ?>
--- navbar.php
+++ navbar.php
@@ -1,13 +1,21 @@
 <nav>
     <ul>
-      <li class='left'><a href="index.php"><img src="images/logo-head.png" alt="logo"></a></li>
-      <li class='right'><a href="login.php">Sign In</a></li>
-      <li class='right'><a href="about.php">About Us</a></li>
-      <li class='right'><a href="order.php">Order Now</a></li>
-      <li class='right'><a href="menu.php">Menu</a></li>
-      <li class='right'><a href="index.php">Home</a></li>
+    	<li class='left'><a href="index.php"><img src="images/logo-head.png" alt="logo"></a></li>
+    	<li class='right'><a href="login.php">Sign In</a></li>
+    	<li class='right'><a href="about.php">About Us</a></li>
+    	<li class='right'><a href="order.php">Order Now</a></li>
+    	<li class='right'><a href="menu.php">Menu</a></li>
+    	<li class='right'><a href="index.php">Home</a></li>
     </ul>
+    <div id="pad"></div>
  </nav>
+ <div id="social">
+    <ul>
+	    <li class="right secondary"><img src="images/twitter.png" alt="twitter" class="social right"></li>
+	    <li class="right secondary"><img src="images/facebook.png" alt="facebook" class="social right"></li>
+	    <li class="right secondary"><img src="images/yelp.png" alt="yelp" class="social right"></li>
+    </ul>
+  </div>
 <script type="text/javascript" src="js/jquery-1.12.3.min.js"></script>
 <script>
         $(function() {
