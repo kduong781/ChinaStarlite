32b7dfde1e9c965017e4ace3c02b2d4feafe4f11
Kevin Duong <kduong5@yahoo.com>
2016-05-04 15:15:55 -0700

    integrated database menu into order page

--- js/order.js
+++ js/order.js
@@ -1,26 +1,123 @@
 $(document).ready(function() {
-$("#lunchTab").click(function(){
-  $("#lunchTab").removeClass("active");
-  $("#dinnerTab").removeClass("active");
-  $("#appetizerTab").removeClass("active");
-  $("#lunchTab").addClass("active");
-  var content = $("#lunch").html();
-  $(".content").replaceWith('<div class="content">'+ content +'</div>');
-});
-$("#dinnerTab").click(function(){
-  $("#lunchTab").removeClass("active");
-  $("#dinnerTab").removeClass("active");
-  $("#appetizerTab").removeClass("active");
-  $("#dinnerTab").addClass("active");
-  var content = $("#dinner").html();
+
+  $("#ChickenTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#Chicken").addClass("active");
+    var content = $("#Chicken").html();
     $(".content").replaceWith('<div class="content">'+ content +'</div>');
-});
-$("#appetizerTab").click(function(){
-    $("#lunchTab").removeClass("active");
-    $("#dinnerTab").removeClass("active");
-    $("#appetizerTab").removeClass("active");
-    $("#appetizerTab").addClass("active");
-    var content = $("#appetizer").html();
+  });
+
+  $("#BeefTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#BeefTab").addClass("active");
+    var content = $("#Beef").html();
     $(".content").replaceWith('<div class="content">'+ content +'</div>');
-});
+  });
+
+  $("#PorkTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#PorkTab").addClass("active");
+    var content = $("#Pork").html();
+    $(".content").replaceWith('<div class="content">'+ content +'</div>');
+  });
+
+  $("#DrinksTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#DrinksTab").addClass("active");
+    var content = $("#Drinks").html();
+    $(".content").replaceWith('<div class="content">'+ content +'</div>');
+  });
+
+  $("#SeafoodTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#SeafoodTab").addClass("active");
+    var content = $("#Seafood").html();
+    $(".content").replaceWith('<div class="content">'+ content +'</div>');
+  });
+
+  $("#SoupTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#SoupTab").addClass("active");
+    var content = $("#Soup").html();
+    $(".content").replaceWith('<div class="content">'+ content +'</div>');
+  });
+
+  $("#AppetizerTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#AppetizerTab").addClass("active");
+    var content = $("#Appetizer").html();
+    $(".content").replaceWith('<div class="content">'+ content +'</div>');
+  });
+
+  $("#viewTab").click(function(){
+    $("#ChickenTab").removeClass("active");
+    $("#BeefTab").removeClass("active");
+    $("#PorkTab").removeClass("active");
+    $("#DrinksTab").removeClass("active");
+    $("#SeafoodTab").removeClass("active");
+    $("#SoupTab").removeClass("active");
+    $("#AppetizerTab").removeClass("active");
+    $("#viewTab").removeClass("active");
+    $("#viewTab").addClass("active");
+    var chicken = $("#Chicken").html();
+    var beef = $("#Beef").html();
+    var pork = $("#Pork").html();
+    var drink = $("#Drinks").html();
+    var seafood = $("#Seafood").html();
+    var soup = $("#Soup").html();
+    var appetizer = $("#Appetizer").html();
+    var content = chicken + beef + pork + drink + seafood + soup + appetizer;
+
+    $(".content").replaceWith('<div class="content">'+ content +'</div>');
+  });
+
 });
--- order.php
+++ order.php
@@ -1,5 +1,8 @@
 #!/usr/local/php5/bin/php-cgi
 <!DOCTYPE html>
+<?php
+  $connection = mysqli_connect("cecs-db01.coe.csulb.edu","cecs323o29","nijeek","cecs323o29");
+?>
 <html lang="en">
 <head>
 	<title>Order Now</title>
@@ -13,71 +16,246 @@
 <div class="wrapper">
 	<div class="leftCol">
 		<ul class="tabs">
-			<li id="lunchTab" class="active">Lunch</li>
+			<?php
+					$sql = "SELECT * FROM cecs323o29.menu";
+					$result = mysqli_query($connection, $sql);
+					if ($result=mysqli_query($connection,$sql))
+					{
+					// Fetch one and one row
+					while ($row=mysqli_fetch_assoc($result))
+						{
+							$type = $row["menutype"];
+							if($row[menutype] == "Chicken") {
+									echo "<li id='".$type."Tab' class='active'>".$type."</li>";
+							} else {
+								echo "<li id='".$type."Tab'>".$type."</li>";
+							}
+						}
+						mysqli_free_result($result);
+					}
+			 ?>
+			 <li id="viewTab">View All</li>
+	<!--		<li id="lunchTab" class="active">Lunch</li>
 			<li id="dinnerTab" >Dinner</li>
-			<li id="appetizerTab" >Appetizer</li>
+			<li id="appetizerTab" >Appetizer</li>-->
 		</ul>
 	</div>
 	<div class="content">
 		<div class="menu">
-			<span class="menuTitle">Lunch</span>
-			<p>Lunch is 20:00 AM - 2:00 PM</p>
-			<ul>
-				<li>
-					<div>
-						<h4>Fried Rice</h4>
-						<p>Award winning rice with eggs, green onions, etc.</p>
-					</div>
-				</li>
-			</ul>
+			<span class="menuTitle">Chicken</span>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Chicken'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
 		</div>
 	</div>
 
 </div>
 
-	<div id="lunch" class="hide">
+
+
+<!-- Tab Info -->
+
+	<div id="Chicken" class="hide">
 		<div class="menu">
-			<span class="menuTitle">Lunch</span>
-			<p>Lunch is 10:00 AM - 2:00 PM</p>
-			<ul>
-				<li>
-					<div>
-						<h4>Fried Rice</h4>
-						<p>Award winning rice with eggs, green onions, etc.</p>
-				 	</div>
-				</li>
-			</ul>
+			<span class="menuTitle">Chicken</span>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Chicken'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
 	  </div>
 	</div>
-	<div id="dinner" class="hide">
+
+	<div id="Beef" class="hide">
 		<div class="menu">
 			<span class="menuTitle">Dinner</span>
-			<p>Dinner is 3:00 PM - 10:00 PM</p>
-			<ul>
-				<li>
-					<div>
-						<h4>Mongolian Beef</h4>
-						<p>Tasty beef with sauce</p>
-					</div>
-				</li>
-			</ul>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Beef'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
 	  </div>
 	</div>
-	<div id="appetizer" class="hide">
+
+	<div id="Pork" class="hide">
 		<div class="menu">
-			<span class="menuTitle">Appetizer</span>
-			<p>Appetizer is all day</p>
-			<ul>
-				<li>
-					<div>
-						<h4>Shrimp Balls</h4>
-						<p>Very good shrimp</p>
-					</div>
-				</li>
-			</ul>
+			<span class="menuTitle">Pork</span>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Pork'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
 		</div>
 	</div>
 
+	<div id="Drinks" class="hide">
+		<div class="menu">
+			<span class="menuTitle">Drinks</span>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Drinks'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
+	  </div>
+	</div>
+
+	<div id="Seafood" class="hide">
+		<div class="menu">
+			<span class="menuTitle">Seafood</span>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Seafood'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
+	  </div>
+	</div>
+
+	<div id="Soup" class="hide">
+		<div class="menu">
+			<span class="menuTitle">Soup</span>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Soup'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
+		</div>
+	</div>
+
+	<div id="Appetizer" class="hide">
+		<div class="menu">
+			<span class="menuTitle">Appetizer</span>
+			<dl>
+				<?php
+						$sql = "SELECT * FROM cecs323o29.menu WHERE menutype='Appetizer'";
+						$result = mysqli_query($connection, $sql);
+						if ($result=mysqli_query($connection,$sql))
+						{
+						// Fetch one and one row
+						while ($row=mysqli_fetch_assoc($result))
+							{
+								$type = $row["menutype"];
+								$name = $row["foodName"];
+								$price = $row["price"];
+								$description = $row["description"];
+								echo "<dt>".$name." <span class='price'>". $price ."</span></dt>";
+								echo "<dd>".$description . "</dd>";
+							}
+							mysqli_free_result($result);
+						}
+				 ?>
+			</dl>
+		</div>
+	</div>
+
+
+<!-- End of Tab Info -->
 <script src="js/order.js"></script>
 <?php include 'footer.php' ?>
 </body>
