28359ec005728bd4bf9751f653c34a3aba5ed6d4
ibreaker14 <minijordon@gmail.com>
2016-05-10 01:07:44 -0700

    fully functional members page with settings and stats

--- member.php
+++ member.php
@@ -247,11 +247,150 @@ var usernames = [];
 		</div><!-- end deactivatebox -->
 
 </div><!-- end settingscontent -->
+
+<!-- =========================================== Start progress ================================================== -->
+
+<h3 class='clear'>Order History:</h3>
+<?php
+	$basicDealAmount = 15;
+	$premiumDealAmount = 40;
+	$deluxeDealAmount = 75;
+	$masterDealAmount = 120;
+	$currentDeal = '';
+ 	$progressPercentage = 0;
+ 	$pointsleft = 0;
+?>
+<div id="leftBarr">
+	<?php 
+	 $sql = "SELECT `date`	
+		FROM orders o
+		LEFT OUTER JOIN users u
+		on u.id=o.userID
+		WHERE username = '$currentusername'
+		GROUP BY `date`;"; //outputs dates
+     $result=mysqli_query($connection,$sql);
+     $dates = array();
+      	if ($result=mysqli_query($connection,$sql)){
+		    while ($row=mysqli_fetch_assoc($result)){
+		    	$dates[] = $row['date'];
+		    }
+		}
+	mysqli_free_result($result);
+	/////////////////////////////////////////////////////////////////////
+	$totalpoints = 0;
+	for($numDate=0;$numDate<sizeof($dates);$numDate++){
+		echo "<div class='floatingBoxLeft'>";
+		echo "<h4>".$dates[$numDate]."<span class='right'>Points</span></h4><ul>";
+		 $sql="SELECT `date`,`quantity`,`foodName`,`price`,`point`,`category` 
+				FROM orders o
+				INNER JOIN users u
+				on u.id=o.userID
+				INNER JOIN menu m
+				on m.menuid=o.menu_menuid
+				WHERE username='$currentusername' AND `date` = '$dates[$numDate]';";
+	     $result=mysqli_query($connection,$sql);
+
+		 if ($result=mysqli_query($connection,$sql)){
+
+		    while ($row=mysqli_fetch_assoc($result)){
+		    	$subpoints = $row['point']*$row['quantity'];
+		    	$food  = ($row['quantity']>1) ? ($row['foodName'].'s') : ($row['foodName']);
+		    	echo "<li>".$row['quantity'].' '.$food." <span class='right'>".$subpoints.'</span></li>';
+		    	$totalpoints+= $subpoints;
+		  	}
+		 }
+		mysqli_free_result($result);
+		echo "</ul></div>";//end floatingboxright
+ 	} //end for numDate
+
+ 	echo "<div class='floatingBoxLeft totalpoints'><p> Total Points: <span class='right'>".$totalpoints."</span></p></div>";
+ 	if($totalpoints >= 0){
+ 		$currentDeal = 'Basic Deal';
+ 	}if($totalpoints >= 16){
+ 		$currentDeal = 'Premium Deal';
+ 	}if($totalpoints >= 41){
+ 		$currentDeal = 'Deluxe Deal';
+ 	}if($totalpoints >= 76){
+ 		$currentDeal = 'Master Deal';
+ 	}
+
+ 	if($currentDeal == 'Basic Deal'){
+ 		$progressPercentage = floor($totalpoints/$basicDealAmount*100);
+ 		$pointsleft = $basicDealAmount - $totalpoints;
+ 	}else if($currentDeal == 'Premium Deal'){
+ 		$progressPercentage = floor($totalpoints/$premiumDealAmount*100);
+ 		$pointsleft = $premiumDealAmount - $totalpoints;
+ 	}else if($currentDeal == 'Deluxe Deal'){
+ 		$progressPercentage = floor($totalpoints/$deluxeDealAmount*100);
+ 		$pointsleft = $deluxeDealAmount - $totalpoints;
+ 	}else if($currentDeal =='Master Deal'){
+ 		$progressPercentage = floor($totalpoints/$masterDealAmount*100);
+ 		$pointsleft = $masterDealAmount - $totalpoints;
+ 	}
+?>
+ 	<div class="floatingboxLeft announcements">
+ 		<h4>Announcements</h4>
+ 		<p><img src="images/deal1.jpg" alt="special deal" class='left'>We are now offering a deal of 2 meals for the price of the most expensive of the 2! This offer is available only to our restaurant members.<br><br>
+ 		<span class='right'>This offer expires May 26, 2016. &nbsp;</span></p>
+ 		<br>
+ 	</div>
+</div><!-- end left -->
+<div id="rightBarr">
+	<div class="floatingBoxRight progress">
+		<h3>Your progress:</h3>
+			You are <?php echo $pointsleft; ?> points away from your next deal!
+		</p>
+		<div id="progressbar">
+			<div id="progress">
+				<div id="light">
+					<p id="progressAmount"><?php echo $progressPercentage.'%'; ?></p>
+				</div><!-- end light -->
+			</div>
+		</div><!-- end progressbar -->
+		<div id="dealName"><?php echo $currentDeal; ?></div><!-- end dealName -->
+	</div><!-- end floatingBoxRight progress -->
+	<div class="floatingBoxRight dealBreakdown">
+		<h3>Deals:</h3>
+		<p class='dealdescription'>Collect points by ordering with us or buying from our restaurant to get your hands on some great deals!</p>
+		<small class="red">*Deals not redeemable for cash</small>
+		<br>
+		<h4>
+			Basic Deal: 15 points
+		</h4>
+		<p>
+			1 free drink<br>
+			1 free meal up to $10
+		</p>
+		<h4>Premium Deal: 40 points</h4>
+		<p>
+			1 free drink<br>
+			free meal(s) up to $20
+		</p>
+		<h4>Deluxe Deal: 75 points</h4>
+		<p>
+			2 free drinks<br>
+			free meal(s) up to $50
+			1 free appetizer of choice
+		</p>
+		<h4>Master Deal</h4>
+		<p>
+			2 free drinks
+			4 meals of your choice
+			2 appetizers of your choice
+			<br>
+			<small>*unclaimed meals can be saved up for next time</small>
+		</p>
+
+	</div><!-- end floatingBoxRightdealBreakdown -->
+</div><!-- end right -->
 </div><!-- end content -->
 </main>
 </div><!-- end wrapper -->
+<script>
+	var progressbar = document.getElementById('progress');
+	progressbar.style.width = "<?php echo $progressPercentage.'%' ?>";
+</script>
 <script src='js/settings.js'></script>
-<?php   mysqli_free_result($result); ?>
 <?php mysqli_close($connection); ?>
 <?php include 'footer.php'; ?>
 </body>
